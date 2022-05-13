b223ac77145d744ec38e8d37368a77f1f8902195
Luca Leon Happel <luca.happel@uni-duesseldorf.de>
2022-05-13 14:42:04 +0200

    following ToshioCP/Gtk4-tutorial

--- src/main.c
+++ src/main.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
-int main (int argc, char *argv[])
-{
-  printf("hallo\n");
-  return 0;
+int main (int argc, char *argv[]) {
+  GtkApplication *app;
+  int stat;
+  app = gtk_application_new ("com.github.Quoteme.windowmenu", G_APPLICATION_FLAGS_NONE);
+  stat = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+  return stat;
 }
